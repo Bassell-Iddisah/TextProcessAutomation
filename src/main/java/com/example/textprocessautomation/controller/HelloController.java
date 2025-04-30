f999fb5d78e4584f31099f86811b039920ea09db
Bassell Iddisah <bbasssell16@gmail.com>
2025-04-30 16:28:28 +0000

    Add distince modules

--- src/main/java/com/example/textprocessautomation/controller/HelloController.java
+++ src/main/java/com/example/textprocessautomation/controller/HelloController.java
@@ -1,14 +1,97 @@
 package com.example.textprocessautomation.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button exportToFileButton;
+
+    @FXML
+    private Label fileLocationText;
+
+    @FXML
+    private Button fileSubmitButton;
+
+    @FXML
+    private Button findAllPatternButton;
+
+    @FXML
+    private Button findPatternButton;
+
+    @FXML
+    private Button matchPatternButton;
+
+    @FXML
+    private TextArea operationalTextField;
+
+    @FXML
+    private TextField regexField;
+
+    @FXML
+    private Button replacePatternButton;
+
+    @FXML
+    private TextArea resultingTextField;
+
+    @FXML
+    private Button selectFileButton;
+
+    @FXML
+    private ComboBox<?> selectOperationComboBox;
+
+    @FXML
+    private Button submitPatternButton;
+
+    @FXML
+    void exportToFileButtonHandler(ActionEvent event) {
+
     }
+
+    @FXML
+    void fileSubmitButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void findAllPatternButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void findPatternButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void matchPatternButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void replacePatternButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectFileButtonHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void selectOperationComboBoxHandler(ActionEvent event) {
+
+    }
+
+    @FXML
+    void submitPatternButtonHandler(ActionEvent event) {
+
+    }
+
 }
---
+++ src/main/java/com/example/textprocessautomation/exceptions/NullFieldException.java
@@ -0,0 +1,7 @@
+package com.example.textprocessautomation.exceptions;
+
+public class NullFieldException extends RuntimeException {
+    public NullFieldException(String message) {
+        super(message);
+    }
+}
