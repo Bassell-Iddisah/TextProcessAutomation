8f866336de1cb5ea1359001d4bff4e381275e974
Bassell Dari Iddisah <bbasssell16@gmail.com>
2025-05-01 17:03:47 +0000

    Add classes to packages

--- src/main/java/com/example/textprocessautomation/exceptions/InvalidFieldException.java
+++
@@ -1,7 +0,0 @@
-package com.example.textprocessautomation.exceptions;
-
-public class InvalidFieldException extends RuntimeException {
-    public InvalidFieldException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/textprocessautomation/exceptions/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.textprocessautomation.exceptions;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
